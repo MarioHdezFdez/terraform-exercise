ef3e24fc4b600d20d632dea68ec64a2cad48797d
mario <Mariof18@gmail.com>
2025-04-10 15:39:56 +0200

    Versión v1.0: Configuración inicial de Terraform

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+}
+
